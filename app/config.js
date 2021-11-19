45d8c501c74f770abfc89b0c1174cc92e7237036
tomf87 <tfrankland@gmail.com>
2021-11-19 14:58:33 +0000

    Updated roadmap

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Defra UCD Roadmap',
+  serviceName: 'Defra user-centred design',
 
   // Default port that prototype runs on
   port: '3000',
